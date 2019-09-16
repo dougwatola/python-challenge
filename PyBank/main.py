6a969e19e7ac518a36e8378cddf7b34847ad0fc0
Doug Watola <dougwatola@gmail.com>
2019-09-16 18:49:47 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,6 +12,7 @@ def average(list):
 
 #Set the path for input budget file
 budgetpath = os.path.join("C:/Users/dougw/Python-Challenge/", "budget_1.csv")
+outPath = os.path.join("C:/Users/dougw/Python-Challenge/", "budgetanalysis.txt")
 #=======================================================
 #Read budget csv file and derive sum of profit and average profit
 #=======================================================
